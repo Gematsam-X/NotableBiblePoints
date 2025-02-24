26b6480afe3cd21df37d49b73ffb3fb7a98c5714
Gionatan Venturi <gionatan.venturi@gmail.com>
2025-02-24 17:50:58 +0100

    feat: init the notes logic. Need more work on this

--- src/js/backendlessInit.js
+++ src/js/backendlessInit.js
@@ -1,3 +1,4 @@
 const appId = "088E9795-83AE-44EB-B266-9A1C5B95F120";
 const apiKey = "E872E3E7-DE72-4126-9EAD-E17A9A8C9249";
-Backendless.initApp(appId, apiKey);
+
+Backendless.initApp(appId, apiKey)
