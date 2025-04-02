e1fe5bed180d91b048c978cd0ed25188444a8867
Gionatan Venturi <gionatan.venturi@gmail.com>
2025-04-02 17:00:59 +0200

    feat(pwa): attempt 4: fix the sw path in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("../../service-worker.js")
       .then((registration) => {
         console.log("Service Worker registrato con successo: ", registration);
       })
