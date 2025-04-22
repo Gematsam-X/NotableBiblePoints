db1b1f9de6dd8f72b5db5d66bcd5493cbe23ac33
Gionatan Venturi <gionatan.venturi@gmail.com>
2025-04-22 18:35:11 +0200

    fix: moved the hardRefresh function within the checkVersion function allowing use

--- src/js/checkVersion.js
+++ src/js/checkVersion.js
@@ -1,24 +1,40 @@
 export default async function checkVersion() {
-    document.addEventListener("DOMContentLoaded", async () => {
+  async function hardRefresh() {
+    if ("caches" in window) {
       try {
-        const response = await fetch("../../version.json", { cache: "no-store" });
-        const data = await response.json();
-        const latestVersion = data.version;
-        const currentVersion = localStorage.getItem("appVersion");
-  
-        if (currentVersion && currentVersion !== latestVersion) {
-          console.log(
-            `Nuova versione disponibile! (${currentVersion} → ${latestVersion})`
-          );
-          localStorage.setItem("appVersion", latestVersion);
-          await hardRefresh();
-        } else if (!currentVersion) {
-          localStorage.setItem("appVersion", latestVersion);
-        } else {
-          console.log("Versione aggiornata, nessuna azione necessaria");
-        }
+        const cacheNames = await caches.keys();
+        await Promise.all(cacheNames.map((cache) => caches.delete(cache)));
+        console.log("Cache eliminata! Ora ricarico la pagina...");
+        window.location.reload();
       } catch (err) {
-        console.error("Errore durante il controllo della versione:", err);
+        console.error("Errore nella cancellazione cache:", err);
+        window.location.reload();
       }
-    });
+    } else {
+      console.warn("Il browser non supporta le cache. Faccio solo il reload.");
+      window.location.reload();
+    }
   }
+  document.addEventListener("DOMContentLoaded", async () => {
+    try {
+      const response = await fetch("../../version.json", { cache: "no-store" });
+      const data = await response.json();
+      const latestVersion = data.version;
+      const currentVersion = localStorage.getItem("appVersion");
+
+      if (currentVersion && currentVersion !== latestVersion) {
+        console.log(
+          `Nuova versione disponibile! (${currentVersion} → ${latestVersion})`
+        );
+        localStorage.setItem("appVersion", latestVersion);
+        await hardRefresh();
+      } else if (!currentVersion) {
+        localStorage.setItem("appVersion", latestVersion);
+      } else {
+        console.log("Versione aggiornata, nessuna azione necessaria");
+      }
+    } catch (err) {
+      console.error("Errore durante il controllo della versione:", err);
+    }
+  });
+}
