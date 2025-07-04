dfd07c5eae0f41f98b262e0db4b1d1712614f659
Gionatan Venturi <gionatan.venturi@gmail.com>
2025-07-04 12:47:53 +0200

    fix: update sw path

--- src/js/main-ok.js
+++ src/js/main-ok.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("/sw.js")
       .then((registration) => {
         console.log("Service Worker registrato con successo: ", registration);
       })
