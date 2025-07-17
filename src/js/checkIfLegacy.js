6f8ce99c9307622b5f5ec61e8225dced780a9836
Gionatan Venturi <gionatan.venturi@gmail.com>
2025-07-17 17:06:33 +0200

    force updating of values in localstorage#7

---
+++ src/js/checkIfLegacy.js
@@ -0,0 +1,13 @@
+import { logoutUser } from "/src/js/logoutAndDelete.js";
+const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+console.log(
+  "risultato del test",
+  regex.test(localStorage.getItem("userEmail"))
+);
+if (regex.test(localStorage.getItem("userEmail"))) {
+  const selectedTheme = localStorage.getItem("theme");
+  localStorage.clear();
+  localStorage.setItem("theme", selectedTheme);
+  localStorage.setItem("appVersion", "2.3.2");
+  await logoutUser(false);
+}
