6cdcd8a721da371873c9193ef18e2b3118783fe9
Gionatan Venturi <gionatan.venturi@gmail.com>
2025-09-09 20:07:37 +0200

    debug for the books #3

--- src/js/main-ok.js
+++ src/js/main-ok.js
@@ -12,30 +12,32 @@ document
   );
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Nel listener di DOMCONTENTLOADED del click sul libro");
-  const booksCells = document.querySelectorAll(".periodic-table td");
-  console.log("booksCells:", booksCells);
-  console.log("booksCells è lungo:", booksCells.length);
-
-  booksCells.forEach((cell) => {
-    console.log("nel foreach delle bookscell. Cella attuale:", cell);
-    cell.addEventListener("click", () => {
-      console.log("Cliccato sulla cella:", cell);
-      try {
-        toast("Libro selezionato: " + cell.dataset.name);
-        const bookName = cell.dataset.name; // Prendiamo il nome dal data-name
-        sessionStorage.setItem("selectedBook", bookName); // Salviamo in sessionStorage
-        console.log("Ecco l'interno sessionStorage:", sessionStorage);
-        console.log(
-          "Ed ecco quello che si ottiene provando a prendere il selecetedBook:",
-          sessionStorage.getItem("selectedBook")
-        );
-        window.location.href = "/src/html/chapters.html"; // Reindirizziamo alla pagina dei capitoli
-      } catch (e) {
-        console.error("Errore durante la selezione del libro:", e);
-      }
+setInterval(() => {
+    console.log("Nel listener di DOMCONTENTLOADED del click sul libro");
+    const booksCells = document.querySelectorAll(".periodic-table td");
+    console.log("booksCells:", booksCells);
+    console.log("booksCells è lungo:", booksCells.length);
+  
+    booksCells.forEach((cell) => {
+      console.log("nel foreach delle bookscell. Cella attuale:", cell);
+      cell.addEventListener("click", () => {
+        console.log("Cliccato sulla cella:", cell);
+        try {
+          toast("Libro selezionato: " + cell.dataset.name);
+          const bookName = cell.dataset.name; // Prendiamo il nome dal data-name
+          sessionStorage.setItem("selectedBook", bookName); // Salviamo in sessionStorage
+          console.log("Ecco l'interno sessionStorage:", sessionStorage);
+          console.log(
+            "Ed ecco quello che si ottiene provando a prendere il selecetedBook:",
+            sessionStorage.getItem("selectedBook")
+          );
+          window.location.href = "/src/html/chapters.html"; // Reindirizziamo alla pagina dei capitoli
+        } catch (e) {
+          console.error("Errore durante la selezione del libro:", e);
+        }
+      });
     });
-  });
+}, 20000);
 });
 
 // Controllo se il browser supporta i service worker
